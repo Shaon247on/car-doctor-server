f644fe83e11e033d26b3c271327dfc04383353db
Md. Aminul Islam Shaon <aminul007.ais@gmail.com>
2024-05-06 22:57:24 +0600

    added the booking collection and set app.post

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ async function run() {
     await client.connect();
 
     const serviceCollection =client.db('carDoctor').collection('services')
-
+    const bookingCollection = client.db('carDoctor').collection('bookings')
     app.get('/services', async(req, res)=>{
         const cursor = serviceCollection.find()
         const result = await cursor.toArray()
@@ -50,11 +50,19 @@ async function run() {
 
         const options = {            
             // Include only the `title` and `imdb` fields in the returned document
-            projection: { title: 1, price: 1 },
+            projection: { title: 1, price: 1, service_id: 1, img: 1 },
           };
 
-        const result= await serviceCollection.findOne(query)
+        const result= await serviceCollection.findOne(query, options)
         res.send(result)
+    })
+
+
+    //bookings
+
+    app.post('/bookings', async(req, res)=>{
+      const booking = req.body
+      console.log(booking)
     })
 
     // Send a ping to confirm a successful connection
